e3964e0e64244ebe3607738de3b36cc09a44b955
Dana Sandman <81288860+DanaSandman@users.noreply.github.com>
2022-02-27 01:17:34 +0200

    days

--- scraping.js
+++ scraping.js
@@ -18,69 +18,108 @@ const csv = createCSV({
     ],
   });
 
- for (let i = 1; i < 11; i++) {
-    axios.get(`https://etherscan.io/blocks?ps=100&p=${i}`)
-        .then( res => {
+let gLastDate = 0
+  
+let gData = [];
 
-            let data = [];
-            const $ = cheerio.load(res.data);
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-            $('tr').each((index, element) => {
-                //block
-                const block = $(element)
-                .children('td:nth-child(1)')
-                .text()
-                // console.log(block);
+let i = 1
 
-                //date
-                const date = $(element)
-                .children('.showDate')
-                .text()
-                // console.log(date);
+async function scarp(){
+  
+    console.log('start', i, 'date', gLastDate, 'length', gData.length);
 
-                //txn
-                const txn = $(element)
-                .children('td:nth-child(4)')
-                .text()
-                // console.log(txn);
-
-                //gasUsed
-                const gasUsed = $(element)
-                .children('td:nth-child(7)')
-                .text()
-                // console.log(gasUsed);
-
-                //gasLimit
-                const gasLimit = $(element)
-                .children('td:nth-child(8)')
-                .text()
-                // console.log(gasLimit);
-
-                //baseFee
-                const baseFee = $(element)
-                .children('td:nth-child(9)')
-                .text()
-                // console.log(baseFee);
-
-                //reward
-                const reward = $(element)
-                .children('td:nth-child(10)')
-                .text()
-                // console.log(reward);
-
-                //burnt fees 
-                const burntFees = $(element)
-                .children('td:nth-child(11)')
-                .text()
-                // console.log(burntFees);   
-
-                data[index] = { block, date, txn, gasUsed, gasLimit, baseFee, reward, burntFees };
+    await sleep(3000);
     
-        });
-        updateCsv()
-        async function updateCsv(){
-            await csv.writeRecords(data)
-              .then(() => { console.log("Done!"); });
-              console.log(data);
-    }}); 
+    let res = await axios.get(`https://etherscan.io/blocks?ps=100&p=${i}`)
+
+    const $ = cheerio.load(res.data);
+
+    $('tr').each(async (index, element) => {
+        //block
+        const block = $(element)
+        .children('td:nth-child(1)')
+        .text()
+        // console.log('block', block);
+
+        //date
+        const date = $(element)
+        .children('.showDate')
+        .text()
+        // console.log('date', date);
+
+        //txn
+        const txn = $(element)
+        .children('td:nth-child(4)')
+        .text()
+        // console.log(txn);
+
+        //gasUsed
+        const gasUsed = $(element)
+        .children('td:nth-child(7)')
+        .text()
+        // console.log(gasUsed);
+
+        //gasLimit
+        const gasLimit = $(element)
+        .children('td:nth-child(8)')
+        .text()
+        // console.log(gasLimit);
+
+        //baseFee
+        const baseFee = $(element)
+        .children('td:nth-child(9)')
+        .text()
+        // console.log(baseFee);
+
+        //reward
+        const reward = $(element)
+        .children('td:nth-child(10)')
+        .text()
+        // console.log(reward);
+
+        //burnt fees 
+        const burntFees = $(element)
+        .children('td:nth-child(11)')
+        .text()
+        // console.log(burntFees);   
+
+        if(block > 0){
+
+          let curDate = new Date(date).getDate();
+
+          // console.log('curDate', curDate);
+          // console.log('gLastDate', gLastDate);
+
+          if(gLastDate === 0){
+            gLastDate = curDate
+          }
+          else if(curDate < gLastDate){
+              console.log('gLastDate', gLastDate);
+              console.log('curDate', curDate);
+              // Add to DB
+              // await csv.writeRecords(gData)
+              console.log('end of the day', gLastDate, 'updateCsv', gData.length);
+              console.log('start block', gData[0].block)
+              console.log('end block', gData[gData.length-1].block);
+              gLastDate = curDate
+              gData = []
+          }
+
+          gData.push({ block, date, txn, gasUsed, gasLimit, baseFee, reward, burntFees });
+              }
+              });
+
+      i++ 
+      if(i<70){
+        scarp()
+
+    }
+
 };
+
+scarp()
+
